f05c78440abb56ea23fc89c8a299137dc0532a17
Rodolfo Santos Silveira <rodolfo360tisilveira@gmail.com>
2019-12-18 23:45:00 -0300

    algumas mudanças

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-transition-link`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
