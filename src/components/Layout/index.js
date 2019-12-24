381fc810e1e70007f010a87a9ea8fe8ab4bbf747
Rodolfo Santos Silveira <rodolfo360tisilveira@gmail.com>
2019-12-24 17:23:45 -0300

    refatorando link

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -9,16 +9,21 @@ import React from "react"
 import PropTypes from "prop-types"
 import Sidebar from "../Sidebar"
 import MenuBar from "../MenuBar"
-import * as S from './styles'
-import GlobalStyles from '../../styles/global'
+import * as S from "./styles"
+import GlobalStyles from "../../styles/global"
+import { TransitionPortal } from "gatsby-plugin-transition-link"
 
 const Layout = ({ children }) => {
   return (
     <S.LayoutWrapper>
       <GlobalStyles />
-      <Sidebar />
+      <TransitionPortal level="top">
+        <Sidebar />
+      </TransitionPortal>
       <S.LayoutMain>{children}</S.LayoutMain>
-      <MenuBar />
+      <TransitionPortal level="top">
+        <MenuBar />
+      </TransitionPortal>
     </S.LayoutWrapper>
   )
 }
